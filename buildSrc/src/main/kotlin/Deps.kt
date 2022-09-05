2729a8039968989d581a37dd7dea2bf9b62c692c
飄然偌僊 <13104871646@163.com>
2022-09-05 18:01:54 +0800

    chore: upgrading jetpackMvvm 2.0.5 to 2.0.6

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -4,7 +4,7 @@ object Deps {
      * FPhoenixCorneaE
      */
     object FPhoenixCorneaE {
-        private const val jetpackMvvm = "com.github.FPhoenixCorneaE:JetpackMvvm:2.0.5"
+        private const val jetpackMvvm = "com.github.FPhoenixCorneaE:JetpackMvvm:2.0.6"
         private const val easyNavigation = "com.github.FPhoenixCorneaE.EasyNavigation:easyNavigation:1.1.0"
         private const val viewPagerAdapter = "com.github.FPhoenixCorneaE:ViewPagerAdapter:1.0.0"
 
